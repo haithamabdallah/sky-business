338ad194d4289fc4883517a2d2583d45a0385164
saied2035 <saied2421998@gmail.com>
2024-07-18 23:11:33 +0300

    prepare font and spacing for contact page next step the form in the contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import { paragraphs } from "./data";
 
 const Contact = () => {
   return (
-    <section className="flex flex-col px-4 mt-[70px] font-futura">
+    <section className="flex flex-col items-center px-4 mt-[70px] font-futura">
       <p className="text-3xl leading-7 mb-12">CONTACT US</p>
       {paragraphs.map((paragraph) => (
         <p className={paragraph.style}>{paragraph.content}</p>
