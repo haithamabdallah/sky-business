9be85637f3b71b1feb77a9d677d909db6dac786b
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-07-25 19:38:23 +0300

    update category

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <Slider />
       <Banners />
       <ProductsByCategory />
-        <ProductList products={products} />
+
       {/* <NewsletterSubscription /> */}
     </main>
   );
--- src/components/home/components/productsByCategory/ProductsByCategory.jsx
+++ src/components/home/components/productsByCategory/ProductsByCategory.jsx
@@ -4,23 +4,25 @@ import { categories } from "./data";
 const ProductsByCategory = () => {
   return (
     <section
-      className="w-full flex flex-wrap justify-center px-0 lg:px-[46px] mt-[66px] 
-      gap-x-[15px] lg:gap-x-[30px] gap-y-[17px] font-futura"
+      className="max-w-[1200px] m-auto flex flex-wrap justify-center px-0 mt-[66px]
+      gap-x-[15px] lg:gap-x-[30px] font-futura pb-[80px]"
     >
-      <p className="w-full text-2xl lg:text-[34px] text-center leading-6 lg:leading-[34px] mb-10">
-        EXPLORE PRODUCTS BY CATEGORY
-      </p>
+      <h2 className="w-full font-normal text-2xl lg:text-[34px] text-center leading-6 lg:leading-[34px] mb-[40px]">
+        EXPLORE brands BY CATEGORY
+      </h2>
       {categories.map((category, i) => (
         <div
           key={`category ${i + 1}`}
-          className="flex flex-col gap-y-[27px] max-w-auto w-[46%] min-[230px]:w-[40%] lg:max-w-[22%] lg:w-auto"
+          className="flex flex-col rounded-[.5625rem] overflow-hidden gap-y-[27px] max-w-auto w-[46%] min-[230px]:w-[40%] lg:max-w-[22.5%] lg:w-auto "
         >
-          <Link to={category.link}>
-            <img src={category.src} alt={category.name} />
+          <Link to={category.link} className="flex flex-col gap-y-[20px] h-zoom-in">
+            <div className="rounded-[.5625rem] overflow-hidden ">
+              <img src={category.src} alt={category.name} />
+            </div>
+            <span className="w-full text-sm text-center leading-[14px] lg:leading-6 font-semibold">
+              {category.name.toUpperCase()}
+            </span>
           </Link>
-          <p className="w-full text-sm text-center leading-[14px] lg:leading-6 font-semibold">
-            {category.name.toUpperCase()}
-          </p>
         </div>
       ))}
     </section>
