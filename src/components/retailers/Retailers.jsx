e9337adcf9dab23e8ae85ac20193bb85715e397d
Ali Reda <alyredagomaa@gmail.com>
2024-07-29 18:43:33 +0300

    edit retailers page

--- src/components/retailers/Retailers.jsx
+++ src/components/retailers/Retailers.jsx
@@ -1,11 +1,19 @@
 import PageCover from '../innerPages/components/pageCover/PageCover';
+import Header from './components/header/Header';
+import Logos from './components/logos/Logos';
 
 const Retail = () => (
-    <PageCover
-        title=""
-        subtitle=""
-        backgroundImage="https://images.unsplash.com/photo-1591375462469-62f189694738?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    />
+    <div className="">
+        <PageCover
+            title=""
+            subtitle=""
+            backgroundImage="https://images.unsplash.com/photo-1591375462469-62f189694738?q=80&w=3348&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <div className="max-w-[90vw] md:max-w-[70vw] mx-auto">
+            <Header />
+            <Logos />
+        </div>
+    </div>
 );
 
 export default Retail;
