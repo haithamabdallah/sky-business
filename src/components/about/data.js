c90e998c0dfdc16a3b889c4d368705c54bc1b502
Ali Reda <alyredagomaa@gmail.com>
2024-07-29 22:14:06 +0300

    add about us page

---
+++ src/components/about/data.js
@@ -0,0 +1,39 @@
+import image1 from './assets/about-us.jpg';
+import image2 from './assets/about-us2.jpg'; 
+
+export const data = {
+    images: [
+        image1,
+        image1,
+        image1,
+        image2,
+        image2,
+        image2,
+    ],
+    text : [
+        `
+            <div className="text-4xl mb-5">RECOMMENDED BY <br /> <strong> DERMATOLOGISTS </strong></div><br />
+            Founded in 1931 by  <strong> Dr. Prosper Haller, </strong>  Vichy is a French brand committed to taking care of sensitive skin. We’ve led the way in skincare innovation for over 90 years. <br />
+            Vichy’s formulas are developed with health professionals and their efficacy are tested under strict <strong> dermatological control </strong>. We are recommended by dermatologists worldwide.
+        `,
+        `
+            <div className="text-4xl mb-5">THE <strong> SKIN HEALTH PARTNER </strong> THAT EMPOWERS WOMEN AT EVERY LIFE STAGE</div>
+            <br /> 
+            All stages of life can visibly affect your skin. When you notice acne, wrinkles, loss of firmness, or any other skin concern related to your life events, our experts will have a solution. Our mission is to help you achieve a healthy skin through every challenges these life stage bring.
+        `,
+        `
+            <div className="text-4xl mb-5">
+                OUR SAFETY PROMISE <br>
+            </div>
+            OUR FORMULATIONS ARE HYPOALLERGENIC, TESTED ON SENSITIVE SKIN AND PARABEN-FREE <br>
+            In addition, we work in close collaboration with a committee of dermatologists & allergy experts to assure the best formula safety. <br>
+            Our factory follows hygiene standards which are on par with the pharmaceutical ones.
+        `,
+            `TESTED ON SENSITIVE SKIN`
+        ,
+            `DERMATOLOGIST RECOMMENDED INGREDIENTS`
+        ,
+            `HYPOALLERGENIC FORMULAS`
+        ,
+    ]
+};
