52cbbe09cd0b373a46dbf947c27ac6841fe1f9a3
saied2035 <saied2421998@gmail.com>
2024-07-31 19:01:55 +0300

    divide code for better readability

--- src/components/organicFoods/components/foodCategories/FoodCategories.jsx
+++ src/components/organicFoods/components/foodCategories/FoodCategories.jsx
@@ -1,35 +1,23 @@
-import { useState } from "react";
-import { foodCategories } from "./data";
+import FoodCategoriesList from "./components/foodCategoriesList/FoodCategoriesList";
 
 const FoodCategories = () => {
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-3 font-futura">
-      {foodCategories.map(({ id, name, src, description }) => (
-        <li key={id} className="relative px-[25px] mb-10">
-          <img src={src} alt={name} className="w-full" />
-          <h2
-            className="absolute text-3xl leading-[0.9] bottom-0 pr-[10px] pl-[20px] pb-[21px]
-          text-white h-[55px] uppercase font-bold"
-          >
-            {name}
-          </h2>
-          <div className="absolute top-0 left-0 w-full h-full px-[25px]">
-            <div
-              className="bg-black w-full h-full text-white
-            flex flex-col opacity-0 px-[25px] py-[15px] justify-center transition-opacity duration-700
-            hover:opacity-100"
-            >
-              <h2 className="text-3xl mb-[5px] leading-none [letter-spacing:0] uppercase font-bold">
-                {name}
-              </h2>
-              <p className="text-base leading-[1.29] pt-[10px] font-sans font-thin">
-                {description}
-              </p>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <section>
+      <h2
+        className="text-center text-5xl mb-[30px] font-bold leading-[48px]
+    uppercase [letter-spacing:_0]"
+      >
+        Products That Your Customers Want
+      </h2>
+      <p
+        className="text-[18px] leading-[26px] text-[#393839] text-center mb-[72px]
+    [letter-spacing:_1.8px] [word-wrap:_break-word] font-sans font-thin"
+      >
+        MDI offers over 40,000 items across ambient, refrigerated, and frozen
+        climate zones, with deep assortments in all product categories.
+      </p>
+      <FoodCategoriesList />
+    </section>
   );
 };
 
