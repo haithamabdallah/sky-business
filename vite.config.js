34da3832031ad243f6af7162a348abde98d8d504
Ali Reda <alyredagomaa@gmail.com>
2024-08-01 18:36:18 +0300

    fix : delete some lines from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['html-react-parser'], // Add this line if externalizing
-    },
-  },
 });
 
