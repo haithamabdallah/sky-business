fe8afcc0034ad1b00ad4cdf99483e3653a04fe3c
saied2035 <saied2421998@gmail.com>
2024-08-04 19:07:18 +0300

    finish link between homepage and dashboard

--- src/components/home/components/skinConsultAIBanner/SkinConsultAIBanner.jsx
+++ src/components/home/components/skinConsultAIBanner/SkinConsultAIBanner.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import BannerImage from "./bannerImage/BannerImage";
 import Description from "./description/Description";
-const SkinConsultAIBanner = () => {
+const SkinConsultAIBanner = ({ banner }) => {
   return (
     <div className="relative">
       <section className="lg:my-[3.75rem] max-w-[75rem] px-[.9375rem] mx-auto mt-[2.1875rem] mb-[4.375rem]">
-        <BannerImage />
-        <Description />
+        <BannerImage image={banner.image} />
+        <Description
+          header={banner.header}
+          buttonText={banner.button_text}
+          text={banner.text}
+        />
       </section>
     </div>
   );
--- src/components/home/components/skinHealthScienceBanner/SkinHealthScienceBanner.jsx
+++ src/components/home/components/skinHealthScienceBanner/SkinHealthScienceBanner.jsx
@@ -3,44 +3,17 @@ import image768 from "./768.jpg";
 import imageRest from "./rest.jpg";
 import imageDefault from "./default.jpg";
 import { Link } from "react-router-dom";
-const SkinHealthScienceBanner = () => {
+const SkinHealthScienceBanner = ({ banner }) => {
+  const url = import.meta.env.VITE_STORAGE_URL;
   return (
-    <section
-      className="max-w-[75rem] px-[.9375rem] mx-auto mb-[60px]"
-    >
-      <picture>
-        <source
-          sizes="1500px"
-          media="(min-width:1024px)"
-          width="1500"
-          height="371"
-          data-aspectratio="4.0431266846361185"
-          srcSet={image1024}
-        />
-        <source
-          sizes="1500px"
-          media="(min-width:768px)"
-          width="1500"
-          height="371"
-          data-aspectratio="4.0431266846361185"
-          srcSet={image768}
-        />
-        <source
-          sizes="600px"
-          media="(min-width:0)"
-          width="600"
-          height="299"
-          data-aspectratio="2.0066889632107023"
-          srcSet={imageRest}
-        />
-        <img
-          alt="Committed to skin health science"
-          className="m-au"
-          src={imageDefault}
-          width="600"
-          height="299"
-        />
-      </picture>
+    <section className="max-w-[75rem] px-[.9375rem] mx-auto mb-[60px]">
+      <img
+        alt="Committed to skin health science"
+        className="m-au"
+        src={`${url}/${banner}`}
+        width="100%"
+        height="299"
+      />
     </section>
   );
 };
