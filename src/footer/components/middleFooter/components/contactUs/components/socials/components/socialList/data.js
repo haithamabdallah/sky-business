afd985b5680bac4a616aac266d1088407f431b15
saied2035 <saied2421998@gmail.com>
2024-08-05 20:59:09 +0300

    fix socials links

--- src/footer/components/middleFooter/components/contactUs/components/socials/components/socialList/data.js
+++ src/footer/components/middleFooter/components/contactUs/components/socials/components/socialList/data.js
@@ -1,13 +1,13 @@
 import facebook from "./assets/facebook.svg";
 import twitter from "./assets/twitter.svg";
 import instagram from "./assets/insta.svg";
-import linkedin from "./assets/linkedin.svg";
+import linkedIn from "./assets/linkedin.svg";
 import whatsApp from "./assets/whatsapp.svg";
 export const data = [
   // './footer/components/middleFooter/components/contactUs/components/socials/components/socialList/assets/linkedin.svg',
   { name: "facebook", src: facebook },
   { name: "twitter", src: twitter },
   { name: "instagram", src: instagram },
-  { name: "linkedin", src: linkedin },
-  { name: "whatsApp", src: linkedin },
+  { name: "linkedin", src: linkedIn },
+  { name: "whatsApp", src: whatsApp },
 ];
