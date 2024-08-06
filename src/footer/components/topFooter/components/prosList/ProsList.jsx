73a1f68f2863a8014fbfe3500778280818cad150
saied2035 <saied2421998@gmail.com>
2024-08-06 22:10:03 +0300

    enhance top footer

--- src/footer/components/topFooter/components/prosList/ProsList.jsx
+++ src/footer/components/topFooter/components/prosList/ProsList.jsx
@@ -12,13 +12,13 @@ const ProsList = () => {
           className="md:px-[15px] px-[10px] flex flex-col mx-auto items-center justify-center"
         >
           <img
-            className="h-[50%]"
+            className="h-[50%] mix-blend-multiply"
             src={src}
             alt={content}
           />
           <span
             className="w-fit lg:text-[0.875rem] lg:leading-[1.25rem] inline-block font-semibold
-              text-[0.75rem] leading-[1rem] align-middle text-left mt-2 text-[#0349ad]"
+              text-[0.75rem] leading-[1rem] align-middle text-left mt-2"
           >
             {content}
           </span>
