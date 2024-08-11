e75cfd5ad33406655142e84bb9e22018535c58c3
Ali Reda <alyredagomaa@gmail.com>
2024-08-11 15:48:34 +0300

    edit middle footer in mobile view

---
+++ src/footer/components/middleFooter/components/middleFooterMobile/components/links/Links.jsx
@@ -0,0 +1,58 @@
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+const Links = ( { data } ) => {
+    return (
+        <div className="">
+            {[data].map(
+                ({ id, h2, ul }) => (
+                    <ul key={id} className={`lg:mt-9 lg:w-1/3 w-full font-futura`}>
+                    <li>
+                        <ul
+                        className={`list-none m-0 lg:mt-[5px] ${
+                            ul[0].src ? "flex gap-x-2" : ""
+                        }`}
+                        >
+                        {ul.map(({ name, route, src }, index) =>
+                            src ? (
+                            <li
+                                key={index}
+                                className={
+                                "border border-[#000] rounded-full flex justify-center items-center px-[6px] py-2" +
+                                `${name == "facebook" ? "scale-[0.50]" : ""}`
+                                }
+                            >
+                                <Link target="_blank" to={`${route}`}>
+                                <img
+                                    src={`${src}`}
+                                    alt={name}
+                                    width={"15px"}
+                                    height={"15px"}
+                                />
+                                </Link>
+                            </li>
+                            ) : (
+                            <li key={index}>
+                                <Link
+                                to={route}
+                                className={`inline-block text-black text-[0.8rem]
+                                    leading-[1rem] font-futura py-[0.3125rem] lg:py-0
+                                    lg:font-normal lg:text-[0.8rem] lg:leading-[1rem] hover:opacity-80 hover:font-[600] ${
+                                        location.pathname === route &&
+                                        "opacity-80 font-[600] underline"
+                                    }`}
+                                >
+                                {name}
+                                </Link>
+                            </li>
+                            )
+                        )}
+                        </ul>
+                    </li>
+                    </ul>
+                )
+            )}
+        </div>
+    )
+}
+
+export default Links
