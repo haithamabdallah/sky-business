2ab38934384668aa87c777e2e3b6084dba0b3595
Ali Reda <alyredagomaa@gmail.com>
2024-08-12 18:51:40 +0300

    fix

--- src/components/unsubscribed/Unsubscribed.jsx
+++ src/components/unsubscribed/Unsubscribed.jsx
@@ -1,4 +1,4 @@
-const Subscribed = () => {
+const Unsubscribed = () => {
     return (
         <div className="">
             <div className="flex flex-col items-center justify-center h-[180px] bg-black"> <h1 className="text-3xl font-bold text-white"> Unsubscribed Successfully </h1> </div>
@@ -8,4 +8,4 @@ const Subscribed = () => {
         </div>
     );
 };
-export default Subscribed;
+export default Unsubscribed;
