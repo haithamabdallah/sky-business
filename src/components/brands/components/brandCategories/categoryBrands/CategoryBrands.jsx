2d58bbc8477e4976c4e159f476986a12ec85c3d2
saied2035 <saied2421998@gmail.com>
2024-08-18 16:17:41 +0300

    enhance brands page animation and style

--- src/components/brands/components/brandCategories/categoryBrands/CategoryBrands.jsx
+++ src/components/brands/components/brandCategories/categoryBrands/CategoryBrands.jsx
@@ -5,13 +5,22 @@ const CategoryBrands = ({ brands }) => {
     <ul className="grid grid-cols-2 min-[400px]:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12">
       {brands.map((brand) => (
         <li className="flex flex-col" key={brand.id}>
-          <img
-            alt={`brand ${brand.id}`}
-            src={`${url}/${brand.image}`}
-            className="cursor-pointer rounded-full transition-transform duration-500 scale-100 hover:scale-125
-            object-contain object-center aspect-square"
-          />
-          <p className="text-center pt-10">{brand.name}</p>
+          <div
+            className="peer cursor-pointer rounded-full transition-transform duration-500
+            scale-100 hover:scale-125 p-8 border border-black border-opacity-50 pee"
+          >
+            <img
+              alt={`brand ${brand.id}`}
+              src={`${url}/${brand.image}`}
+              className="object-contain object-center aspect-square"
+            />
+          </div>
+          <p
+            className="text-center transition-[margin] duration-500 mt-5
+          peer-hover:mt-10"
+          >
+            {brand.name}
+          </p>
         </li>
       ))}
     </ul>
