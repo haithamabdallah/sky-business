58580e8cbf9245e0e06acd13d7465d7dc3282655
saied2035 <saied2421998@gmail.com>
2024-08-19 19:39:38 +0300

    update top footer

--- src/footer/components/topFooter/TopFooter.jsx
+++ src/footer/components/topFooter/TopFooter.jsx
@@ -3,17 +3,11 @@ import ProsList from "./components/prosList/ProsList";
 import { Context } from "../../../ContextProvider";
 
 const TopFooter = () => {
-  const { value } = useContext(Context);
-  const imgDesktop = value.settings.top_footer_desktop;
-  const imgMobile = value.settings.top_footer_mobile;
-  const url = import.meta.env.VITE_STORAGE_URL;
   return (
     <div>
       <div className="w-full mx-auto">
         <div className="w-full mx-auto">
-          <img alt="top footer" src={`${url}/${imgDesktop}`} width="100%" className="hidden md:block" />
-          <img alt="top footer" src={`${url}/${imgMobile}`} width="100%" className="md:hidden" />
-          {/* <ProsList /> */}
+          <ProsList />
         </div>
       </div>
     </div>
--- src/footer/components/topFooter/components/prosList/ProsList.jsx
+++ src/footer/components/topFooter/components/prosList/ProsList.jsx
@@ -1,26 +1,26 @@
-import { list } from "./data";
+import { useContext } from "react";
+import { Context } from "../../../../../ContextProvider";
 
 const ProsList = () => {
+  const { value } = useContext(Context);
+  const list = value.settings.top_footer;
+  const url = import.meta.env.VITE_STORAGE_URL;
   return (
     <ul
-      className="font-futura flex flex-wrap justify-between
-      py-2 border-b bg-[rgb(209_213_219_/1)] gap-y-2"
+      className="font-futura flex flex-wrap justify-around
+       py-5 md:py-10 border-b bg-black gap-y-2"
     >
-      {list.map(({ id, src, content }) => (
+      {list.map(({ text, image }, index) => (
         <li
-          key={id}
+          key={index}
           className="md:px-[15px] px-[10px] flex flex-col mx-auto items-center justify-center"
         >
-          <img
-            className="h-[50%] mix-blend-multiply"
-            src={src}
-            alt={content}
-          />
+          <img className="w-[40px] md:w-[80px]" src={`${url}/${image}`} alt={text} />
           <span
             className="w-fit lg:text-[0.875rem] lg:leading-[1.25rem] inline-block font-semibold
-              text-[0.75rem] leading-[1rem] align-middle text-left mt-2"
+              text-[0.75rem] leading-[1rem] align-middle text-left mt-2 text-white"
           >
-            {content}
+            {text}
           </span>
         </li>
       ))}
