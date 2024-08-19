370b1792c5258fa99ada8a4fb8359931519ee33c
saied2035 <saied2421998@gmail.com>
2024-08-19 14:08:30 +0300

    make brands page as it was before

--- src/components/brands/components/brandCategories/categoryBrands/CategoryBrands.jsx
+++ src/components/brands/components/brandCategories/categoryBrands/CategoryBrands.jsx
@@ -1,24 +1,16 @@
 const CategoryBrands = ({ brands }) => {
   const url = import.meta.env.VITE_STORAGE_URL;
-
   return (
     <ul className="grid grid-cols-2 min-[400px]:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12">
       {brands.map((brand) => (
-        <li className="flex flex-col" key={brand.id}>
-          <div
-            className="peer cursor-pointer rounded-full transition-transform duration-500
-            scale-100 hover:scale-125 p-[16.5%] border border-black border-opacity-50 pee"
-          >
-            <img
-              alt={`brand ${brand.id}`}
-              src={`${url}/${brand.image}`}
-              className="object-contain object-center aspect-square"
-            />
-          </div>
-          <p
-            className="text-center transition-[margin] duration-500 mt-5
-          peer-hover:mt-10"
-          >
+        <li className="flex flex-col gap-x-2" key={brand.id}>
+          <img
+            alt={`brand ${brand.id}`}
+            src={`${url}/${brand.image}`}
+            className="peer cursor-pointer rounded-full transition-transform duration-500 scale-100
+            hover:scale-125 object-contain object-center aspect-square"
+          />
+          <p className="text-center transition-[margin] duration-500 mt-5 peer-hover:mt-10">
             {brand.name}
           </p>
         </li>
@@ -26,5 +18,4 @@ const CategoryBrands = ({ brands }) => {
     </ul>
   );
 };
-
 export default CategoryBrands;
