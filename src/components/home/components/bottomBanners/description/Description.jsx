026f98bbd1734200a0086d5ae57e1fc1d62826c9
saied2035 <saied2421998@gmail.com>
2024-08-20 14:17:52 +0300

    enhance slider styling

--- src/components/home/components/bottomBanners/description/Description.jsx
+++ src/components/home/components/bottomBanners/description/Description.jsx
@@ -18,7 +18,7 @@ const Description = ({
         className={`h-full w-full max-w-[75rem] pointer-events-none  mx-auto
         relative items-center
           font-futura flex ${
-            isSlider || isCover
+            isCover
               ? `md:px-[0.9375rem]
               md:absolute md:top-[50%] md:left-[50%] md:[transform:translate(-50%,-50%)]`
               : `lg:px-[0.9375rem] lg:left-[50%]
@@ -29,7 +29,7 @@ const Description = ({
           className={`mx-0 w-full inline-block lg:py-10
           py-5 ${
             isSlider
-              ? "px-4 text-center md:w-[50%] md:text-left"
+              ? "px-4 text-center lg:w-[50%] lg:text-left"
               : "text-left lg:w-[50%]"
           } ${
             isCover ? "px-4 md:w-[50%]" : "lg:px-[0.9375rem] lg:w-[50%] px-0"
@@ -39,12 +39,12 @@ const Description = ({
             <h2
               className={`[letter-spacing:-.05125rem] pointer-events-auto
             ${
-              isSlider || isCover
+              isCover
                 ? `md:text-[2.125rem] text-[1.5rem] md:leading-[2.125rem] leading-[1.5]
                 md:mb-[.75rem] mb-[0.375rem] md:w-[90%] w-auto`
                 : `lg:text-[2.125rem] text-[1.5rem] lg:leading-[2.125rem] leading-[1.5]
                 lg:mb-[.75rem] mb-[0.375rem] w-auto`
-            }`}
+            } ${isSlider ? "lg:w-[90%] w-auto" : "w-auto"}`}
             >
               {header}
             </h2>
@@ -53,8 +53,8 @@ const Description = ({
             <p
               className={`text-[#000] lg:text-[.875rem] text-[0.75rem] lg:leading-[1.25rem]
               leading-[1rem] pb-[1.625rem] font-futura pointer-events-auto ${
-                isSlider || isCover ? "md:w-[90%] w-auto" : "w-auto"
-              }`}
+                isCover ? "md:w-[90%] w-auto" : "w-auto"
+              } ${isSlider ? "lg:w-[90%] w-auto" : "w-auto"}`}
             >
               {parse(text)}
             </p>
@@ -63,7 +63,7 @@ const Description = ({
             <div>
               <div
                 className={`w-full inline-block relative align-top
-              ${isSlider && "flex justify-center md:block"}`}
+              ${isSlider && "flex justify-center lg:block"}`}
               >
                 <Link
                   to={buttonUrl}
