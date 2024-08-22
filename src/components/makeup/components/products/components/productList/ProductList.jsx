1fbda7c2d99d58f69118902dd8470e0ac5630936
saied2035 <saied2421998@gmail.com>
2024-08-22 20:48:59 +0300

    remove padding from beatuy pages products

--- src/components/makeup/components/products/components/productList/ProductList.jsx
+++ src/components/makeup/components/products/components/productList/ProductList.jsx
@@ -1,11 +1,11 @@
 const ProductList = ({ products }) => {
   const url = import.meta.env.VITE_STORAGE_URL;
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 font-futura">
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[50px] font-futura">
       {products.map(({ id, name, image, text }) => (
         <li
           key={id}
-          className="cursor-pointer relative px-[25px] mb-10 w-full grid grid-rows-5"
+          className="cursor-pointer relative mb-10 w-full grid grid-rows-5"
         >
           <img
             src={`${url}/${image}`}
@@ -14,7 +14,7 @@ const ProductList = ({ products }) => {
           <h2 className="leading-[0.9] text-center mt-10 uppercase font-bold">
             {name}
           </h2>
-          <div className="absolute top-0 left-0 w-full h-full px-[25px]">
+          <div className="absolute top-0 left-0 w-full h-full">
             <div
               className="bg-black w-full h-full text-white
           flex flex-col opacity-0 px-[25px] py-[15px] justify-center transition-opacity duration-700
