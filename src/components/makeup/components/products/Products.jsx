e2f26e75ad81165899703269a96ffe85c87c3190
saied2035 <saied2421998@gmail.com>
2024-08-22 21:25:26 +0300

    edit font in beauty pages

--- src/components/makeup/components/products/Products.jsx
+++ src/components/makeup/components/products/Products.jsx
@@ -4,13 +4,14 @@ const Products = ({ productsData }) => {
   return (
     <section className="flex flex-col items-center px-4">
       <h2
-        className="text-center text-5xl mb-[30px] font-bold leading-[48px]
+        className="text-center text-2xl min-[562px]:text-5xl mb-[30px] font-bold leading-[48px]
     uppercase [letter-spacing:_0]"
       >
         {productsData.header}
       </h2>
       <div
-        className="text-[18px] leading-[26px] text-[#393839] text-center mb-[72px]
+        className="[&_*]:min-[562px]:[font-size:18px_!important] [&_*]:[font-size:16px_!important]
+        leading-[26px] text-[#393839] text-center mb-[72px]
         [letter-spacing:_1.8px] [word-wrap:_break-word] font-sans font-thin"
       >
         {parse(productsData.text)}
