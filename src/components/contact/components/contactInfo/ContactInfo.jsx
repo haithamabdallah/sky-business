3750cdf3e6da040e15bfd65e898f5202b236765f
saied2035 <saied2421998@gmail.com>
2024-08-22 16:47:26 +0300

    change in contact us page

--- src/components/contact/components/contactInfo/ContactInfo.jsx
+++ src/components/contact/components/contactInfo/ContactInfo.jsx
@@ -7,7 +7,8 @@ const ContactInfo = ({ header, text }) => {
       <p className="text-3xl leading-7 mb-5">{header}</p>
       <div
         className="[&_*]:mx-[0px_!important] [&_*]:px-[0px_!important]
-            [&_*]:text-[rgba(0,0,0,0.5)_!important] [&_*]:[font-size:16px_!important]"
+            [&_*]:text-[rgba(0,0,0,0.5)_!important] [&_*]:[font-size:16px_!important] border-b
+            border-[rgba(0,0,0,0.2)] "
       >
         {parse(text)}
       </div>
--- src/components/contact/components/contactInfo/components/contactMethods/ContactMethods.jsx
+++ src/components/contact/components/contactInfo/components/contactMethods/ContactMethods.jsx
@@ -3,6 +3,7 @@ import phoneImage from "./phone-icon.svg";
 import emailImage from "./email-icon.svg";
 import timeImage from "./time-icon.svg";
 import { Context } from "../../../../../../ContextProvider";
+import { Link } from "react-router-dom";
 const ContactMethods = () => {
   const { value } = useContext(Context);
   const {
@@ -11,34 +12,34 @@ const ContactMethods = () => {
     work_hours: workHours,
   } = value.settings;
   return (
-    <div className="flex flex-col gap-y-4">
+    <div className="flex flex-col gap-y-4 mt-5">
       <div className="flex gap-x-4 w-full">
         <img
           className="bg-[#0149ab] w-[32px] h-[32px] p-1 mt-1"
           src={phoneImage}
           alt="phone icon"
         />
-        <div className="flex flex-col">
+        <Link to={`tel:${phone}`} className="flex flex-col">
           <p className="text-sm font-bold">Have any Questions ?</p>
           <p className="text-sm text-[rgba(0,0,0,0.4)]">{phone}</p>
-        </div>
+        </Link>
       </div>
       <div className="flex gap-x-4 w-full">
         <img
           className="bg-[#0149ab] w-[32px] h-[32px] p-1 mt-1"
           src={emailImage}
-          alt="phone icon"
+          alt="email icon"
         />
-        <div className="flex flex-col">
+        <Link to={`mailto:${notificationEmail}`} className="flex flex-col">
           <p className="text-sm font-bold">Write email</p>
           <p className="text-sm text-[rgba(0,0,0,0.4)]">{notificationEmail}</p>
-        </div>
+        </Link>
       </div>
       <div className="flex gap-x-4 w-full items-center">
         <img
           className="bg-[#0149ab] w-[32px] h-[32px] p-1"
           src={timeImage}
-          alt="phone icon"
+          alt="work hours icon"
         />
         <div className="flex flex-col">
           <p className="text-sm font-bold">Visit anytime</p>
