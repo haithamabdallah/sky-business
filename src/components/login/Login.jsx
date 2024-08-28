b31503992f67bf83f9bda5969d45fb517e934bc3
saied2035 <saied2421998@gmail.com>
2024-08-28 19:24:49 +0300

    edit logic pf errors

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -35,6 +35,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setErrors({});
     const response = await sendRequest({
       method: "post",
       endpoint: "login",
