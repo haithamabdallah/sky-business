361898ef5d2ab9096eb4a651f8132e83b0571db4
saied2035 <saied2421998@gmail.com>
2024-08-29 14:45:44 +0300

    return app withput lazy loading because it cause errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,36 @@
-import { useContext, useEffect, useState, lazy, Suspense } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
+import Home from "./components/home/Home";
+import Register from "./components/Register/register";
+import ThankYou from "./components/Register/components/thankYou/ThankYou";
+import Login from "./components/login/Login";
+import About from "./components/about/About";
+import SkinCare from "./components/skinCare/SkinCare";
+import Blog from "./components/blog/Blog";
+import Brands from "./components/brands/Brands";
+import Retailers from "./components/retailers/Retailers";
+import Contact from "./components/contact/Contact";
+import HealthCare from "./components/healthCare/HealthCare";
 import Footer from "./footer/Footer";
+import Article from "./components/blog/components/blogCards/components/article/Article";
 import sendRequest from "./methods/fetchData";
 import logo from "./navbar/logo.jpeg";
 import { Context } from "./ContextProvider";
-import ScrollToTop from "./scrollToTop/ScrollToTop";
+import Makeup from "./components/makeup/Makeup";
+import HairCare from "./components/hairCare/HairCare";
+import Terms from "./components/staticPages/terms/Terms";
+import Privacy from "./components/staticPages/privacy/Privacy";
+import Returns from "./components/staticPages/returns/Returns";
 import Cookies from "./cookies/Cookies";
-import renderPreviousPage from "./renderPreviousPage";
-
-// Lazy load components
-const Home = lazy(() => import("./components/home/Home"));
-const Register = lazy(() => import("./components/Register/register"));
-const ThankYou = lazy(() =>
-  import("./components/Register/components/thankYou/ThankYou")
-);
-const Login = lazy(() => import("./components/login/Login"));
-const About = lazy(() => import("./components/about/About"));
-const SkinCare = lazy(() => import("./components/skinCare/SkinCare"));
-const Blog = lazy(() => import("./components/blog/Blog"));
-const Brands = lazy(() => import("./components/brands/Brands"));
-const Retailers = lazy(() => import("./components/retailers/Retailers"));
-const Contact = lazy(() => import("./components/contact/Contact"));
-const HealthCare = lazy(() => import("./components/healthCare/HealthCare"));
-const Article = lazy(() =>
-  import("./components/blog/components/blogCards/components/article/Article")
-);
-const Makeup = lazy(() => import("./components/makeup/Makeup"));
-const HairCare = lazy(() => import("./components/hairCare/HairCare"));
-const Terms = lazy(() => import("./components/staticPages/terms/Terms"));
-const Privacy = lazy(() => import("./components/staticPages/privacy/Privacy"));
-const Returns = lazy(() => import("./components/staticPages/returns/Returns"));
-const SearchResult = lazy(() => import("./searchResult/SearchResult"));
-const ForgetPassword = lazy(() =>
-  import("./components/forgetPassword/ForgetPassword")
-);
-const Subscribed = lazy(() => import("./components/subscribed/Subscribed"));
-const Unsubscribed = lazy(() =>
-  import("./components/unsubscribed/Unsubscribed")
-);
-const Unsubscribe = lazy(() => import("./components/unsubscribe/Unsubscribe"));
-const ConfirmSubscribe = lazy(() =>
-  import("./components/confirmSubscribe/ConfirmSubscribe")
-);
-const ResetPassword = lazy(() =>
-  import("./components/resetPassword/ResetPassword")
-);
+import SearchResult from "./searchResult/SearchResult";
+import ForgetPassword from "./components/forgetPassword/ForgetPassword";
+import Subscribed from "./components/subscribed/Subscribed";
+import Unsubscribed from "./components/unsubscribed/Unsubscribed";
+import Unsubscribe from "./components/unsubscribe/Unsubscribe";
+import ConfirmSubscribe from "./components/confirmSubscribe/ConfirmSubscribe";
+import ResetPassword from "./components/resetPassword/ResetPassword";
+import ScrollToTop from "./scrollToTop/ScrollToTop";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -65,12 +50,10 @@ const App = () => {
   const [staticPagesData, setStaticPagesData] = useState({});
   const [showMargin, setShowMargin] = useState(true);
   const { pathname } = useLocation();
-
   const isEmailPage = /(\/subscribed|\/unsubscribed|\/unsubscribe)/.test(
     pathname
   );
   const { setValue } = useContext(Context);
-
   useEffect(() => {
     Promise.all([
       sendRequest({ method: "post", endpoint: "settings" }).then((res) => {
@@ -112,14 +95,13 @@ const App = () => {
     });
   }, []);
 
-  const renderLoader = () => (
-    <div className="min-w-[100vw] min-h-[100vh] absolute flex items-center justify-center top-0 z-50 bg-white bg-opacity-50">
+  return loading ? (
+    <div
+      className="min-w-[100vw] min-h-[100vh] absolute flex items-center justify-center
+      top-0 z-50 bg-white bg-opacity-50"
+    >
       <img alt="loading" src={logo} className="animate-breath w-36" />
     </div>
-  );
-
-  return loading ? (
-    renderLoader()
   ) : (
     <>
       {isEmailPage && (
@@ -129,8 +111,9 @@ const App = () => {
           <Route path="/unsubscribe" element={<Unsubscribe />} />
         </Routes>
       )}
-      {!isEmailPage && (
+      {isEmailPage === false && (
         <main className="grid grid-cols-1 gap-0 min-h-[100vh] overflow-x-hidden">
+          <ScrollToTop />
           <div className="self-start">
             {!localStorage.getItem("cookies_popup") && showCookies && (
               <Cookies setShowCookies={setShowCookies} />
@@ -141,96 +124,93 @@ const App = () => {
                 showMargin ? "mt-[54px] min-[1200px]:mt-[110px]" : "mt-0"
               }`}
             >
-              <Suspense fallback={() => renderPreviousPage(pathname)}>
-                <ScrollToTop />
-                <Routes>
-                  <Route
-                    path="/"
-                    element={
-                      <Home
-                        homeData={homeData}
-                        showPopup={showPopup}
-                        setShowPopup={setShowPopup}
-                      />
-                    }
-                  />
-                  <Route path="/search" element={<SearchResult />} />
-                  <Route
-                    path="/register"
-                    element={<Register registerData={registerData} />}
-                  />
-                  <Route path="/thank-you" element={<ThankYou />} />
-                  <Route
-                    path="/login"
-                    element={<Login loginData={registerData} />}
-                  />
-                  <Route path="/forget_password" element={<ForgetPassword />} />
-                  <Route
-                    path="/password-reset/:token"
-                    element={<ResetPassword />}
-                  />
-                  <Route
-                    path="/about"
-                    element={<About aboutData={aboutData} />}
-                  />
-                  <Route
-                    path="/skin_care"
-                    element={<SkinCare skinCareData={skinCareData} />}
-                  />
-                  <Route path="/blog" element={<Blog blogData={blogData} />} />
-                  <Route path="/blog/:id" element={<Article />} />
-                  <Route
-                    path="/brands"
-                    element={<Brands brandData={brandData} />}
-                  />
-                  <Route
-                    path="/retailers"
-                    element={<Retailers retailerData={retailerData} />}
-                  />
-                  <Route
-                    path="/contact"
-                    element={<Contact contactData={contactData} />}
-                  />
-                  <Route
-                    path="/health_care"
-                    element={<HealthCare healthCareData={healthCareData} />}
-                  />
-                  <Route
-                    path="/makeup"
-                    element={<Makeup makeupData={makeupData} />}
-                  />
-                  <Route
-                    path="/hair_care"
-                    element={<HairCare hairCareData={hairCareData} />}
-                  />
-                  <Route
-                    path="/terms"
-                    element={<Terms staticPagesData={staticPagesData} />}
-                  />
-                  <Route
-                    path="/privacy"
-                    element={<Privacy staticPagesData={staticPagesData} />}
-                  />
-                  <Route
-                    path="/returns"
-                    element={<Returns staticPagesData={staticPagesData} />}
-                  />
-                  <Route
-                    path="/confirm-subscribe"
-                    element={<ConfirmSubscribe />}
-                  />
-                  <Route
-                    path="*"
-                    element={
-                      <Home
-                        homeData={homeData}
-                        showPopup={showPopup}
-                        setShowPopup={setShowPopup}
-                      />
-                    }
-                  />
-                </Routes>
-              </Suspense>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Home
+                      homeData={homeData}
+                      showPopup={showPopup}
+                      setShowPopup={setShowPopup}
+                    />
+                  }
+                />
+                <Route path="/search" element={<SearchResult />} />
+                <Route
+                  path="/register"
+                  element={<Register registerData={registerData} />}
+                />
+                <Route path="/thank-you" element={<ThankYou />} />
+                <Route
+                  path="/login"
+                  element={<Login loginData={registerData} />}
+                />
+                <Route path="/forget_password" element={<ForgetPassword />} />
+                <Route
+                  path="/password-reset/:token"
+                  element={<ResetPassword />}
+                />
+                <Route
+                  path="/about"
+                  element={<About aboutData={aboutData} />}
+                />
+                <Route
+                  path="/skin_care"
+                  element={<SkinCare skinCareData={skinCareData} />}
+                />
+                <Route path="/blog" element={<Blog blogData={blogData} />} />
+                <Route path="/blog/:id" element={<Article />} />
+                <Route
+                  path="/brands"
+                  element={<Brands brandData={brandData} />}
+                />
+                <Route
+                  path="/retailers"
+                  element={<Retailers retailerData={retailerData} />}
+                />
+                <Route
+                  path="/contact"
+                  element={<Contact contactData={contactData} />}
+                />
+                <Route
+                  path="/health_care"
+                  element={<HealthCare healthCareData={healthCareData} />}
+                />
+                <Route
+                  path="/makeup"
+                  element={<Makeup makeupData={makeupData} />}
+                />
+                <Route
+                  path="/hair_care"
+                  element={<HairCare hairCareData={hairCareData} />}
+                />
+                <Route
+                  path="/terms"
+                  element={<Terms staticPagesData={staticPagesData} />}
+                />
+                <Route
+                  path="/privacy"
+                  element={<Privacy staticPagesData={staticPagesData} />}
+                />
+                <Route
+                  path="/returns"
+                  element={<Returns staticPagesData={staticPagesData} />}
+                />
+                <Route
+                  path="/confirm-subscribe"
+                  element={<ConfirmSubscribe />}
+                />
+                <Route
+                  path="*"
+                  element={
+                    <Home
+                      homeData={homeData}
+                      showPopup={showPopup}
+                      setShowPopup={setShowPopup}
+                    />
+                  }
+                />
+              </Routes>
             </main>
           </div>
           <div className="self-end">
@@ -241,5 +221,4 @@ const App = () => {
     </>
   );
 };
-
 export default App;
